9f756e70285379ca617ed3e4c01505c322b04917
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-03 01:54:14 +0100

    Update TechnicianListScreen.js

--- client/src/screens/TechnicianListScreen.js
+++ client/src/screens/TechnicianListScreen.js
@@ -1,6 +1,5 @@
 // src/screens/TechnicianListScreen.js
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
 import { getAllTechnicians } from '../services/technicianService';
 import TechnicianCard from '../components/TechnicianCard';
 import { globalStyles } from '../styles/globalStyles';

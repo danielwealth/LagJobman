da69f70057e7a3801fdabe0cf0a52e9e3244232c
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-08 18:03:20 +0100

    Refactor routes and update screen imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import your screens
+// Screens
 import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
+import SignUpScreen from './screens/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';
+import RegisterScreen from './screens/RegisterScreen'; // Technician registration
 import ProfileScreen from './screens/ProfileScreen';
 import SearchScreen from './screens/SearchScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-
+import TechnicianListScreen from './screens/TechnicianListScreen';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* User authentication */}
         <Route path="/" element={<LoginScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
+
+        {/* Technician marketplace (protected routes) */}
         <Route path="/home" element={<HomeScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
         <Route path="/profile" element={<ProfileScreen />} />
         <Route path="/search" element={<SearchScreen />} />
-         <Route path="/Favorites" element={<FavoritesScreen />} />
+        <Route path="/favorites" element={<FavoritesScreen />} />
+        <Route path="/technicians" element={<TechnicianListScreen />} />
       </Routes>
     </Router>
   );

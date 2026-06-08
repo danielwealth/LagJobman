a6eec01c0e0f9dc7ebe13d093142588f982fd51b
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-08 18:04:28 +0100

    Implement protected routes for user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,66 @@ import SearchScreen from './screens/SearchScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
 import TechnicianListScreen from './screens/TechnicianListScreen';
 
+// Components
+import ProtectedRoute from './components/ProtectedRoute';
+
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* User authentication */}
+        {/* Public routes */}
         <Route path="/" element={<LoginScreen />} />
         <Route path="/signup" element={<SignUpScreen />} />
 
-        {/* Technician marketplace (protected routes) */}
-        <Route path="/home" element={<HomeScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-        <Route path="/profile" element={<ProfileScreen />} />
-        <Route path="/search" element={<SearchScreen />} />
-        <Route path="/favorites" element={<FavoritesScreen />} />
-        <Route path="/technicians" element={<TechnicianListScreen />} />
+        {/* Protected routes */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomeScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute>
+              <RegisterScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfileScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <SearchScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <ProtectedRoute>
+              <FavoritesScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/technicians"
+          element={
+            <ProtectedRoute>
+              <TechnicianListScreen />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
